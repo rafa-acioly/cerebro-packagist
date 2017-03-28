c7a7073a83f9b3ab9c134da693a3e742bea9fd5d
Rafael <aciolyr@gmail.com>
2017-03-28 13:45:44 -0300

    open url function removed

--- src/index.js
+++ src/index.js
@@ -3,28 +3,26 @@ var packagist = require('./packagist');
 const icon = require('./icon.png');
 
 const plugin = ({term, display, actions}) => {
-  if (term.match(/^packagist/)) {
-    let param = term.split(' ')[1]
-    packagist(param).then((item) => {
-      item.results.forEach(pack => {
-        display({
-          icon,
-          title: pack.name,
-          subtitle: pack.description,
-          onSelect: () => {
-            actions.open(pack.url)
-            actions.copyToClipboard(`composer install ${pack.name}`)
-            new Notification('Composer command copied!', {body: `composer install ${pack.name}`, icon: icon})
-          },
-          getPreview: () => {
-            return `<h1>${pack.name}</h1>
-            <h2>${pack.description}</h2>
-            <small>Downloads: ${pack.downloads}</small>`
-          }
-        })
+  if (!term.match(/^packagist/)) { return; }
+  let param = term.split(' ')[1]
+  packagist(param).then((item) => {
+    item.results.forEach(pack => {
+      display({
+        icon,
+        title: pack.name,
+        subtitle: pack.description,
+        onSelect: () => {
+          actions.copyToClipboard(`composer install ${pack.name}`)
+          new Notification('Composer command copied!', {body: `composer install ${pack.name}`, icon: icon})
+        },
+        getPreview: () => {
+          return `<h1>${pack.name}</h1>
+          <h2>${pack.description}</h2>
+          <small>Downloads: ${pack.downloads}</small>`
+        }
       })
     })
-  }
+  })
 };
 
 module.exports = {
--- src/packagist.js
+++ src/packagist.js
@@ -1,5 +1,5 @@
 const pack = (tagName) => {
-  let url = `https://packagist.org/search.json?q=${tagName}&per_page=4`;
+  let url = `https://packagist.org/search.json?q=${tagName}&per_page=5`;
   return fetch(url).then(response => response.json());
 }
 
